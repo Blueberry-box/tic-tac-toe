1ab0f1f77f3fb904be28b46f73ff45b50e1c50af
Blueberry-box <245140791+Blueberry-box@users.noreply.github.com>
2026-03-26 16:58:36 +0200

    add interactive user interface

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 const gameboard = (() => {
-    const state = [
+    let state = [
         null, null, null,
         null, null, null,
         null, null, null,
     ];
 
 
-    const printGameState = function () {
-        console.log(state[0],  state[1], state[2]);
-        console.log(state[3],  state[4], state[5]);
-        console.log(state[6],  state[7], state[8]);
+    const resetGame = function () {
+        for (let i = 0; i < state.length; i++) {
+            state[i] = null;
+        }
+
     }
 
 
     const isGameWon = function () {
-        const allEqual = (array) => array.every(element => element === state[0] && state[0] !== null);
-
-        let col1, col2, col3, row1, row2, row3, d1, d2;
-        col1 = [state[0],  state[3], state[6]];
-        col2 = [state[1],  state[4], state[7]];
-        col3 = [state[2],  state[5], state[8]];
-
-        row1 = [state[0],  state[1], state[2]];
-        row2 = [state[3],  state[4], state[5]];
-        row3 = [state[6],  state[7], state[8]];
-
-        d1 = [state[0], state[4], state[8]];
-        d2 = [state[2], state[4], state[6]];
-        if (allEqual(col1) ||
-            allEqual(col2) ||
-            allEqual(col3) ||
-            allEqual(row1) ||
-            allEqual(row2) ||
-            allEqual(row3) ||
-            allEqual(d1) ||
-            allEqual(d2)
-            ) {
+        const allEqual = (array) => {
+            let combo = array.join("");
+            if (combo === "XXX" || combo === "OOO") {
                 return true;
+            } else {
+                return false;
+            }
+        };
+
+        const winningPossibilities = [
+            [state[0],  state[3], state[6]],
+            [state[1],  state[4], state[7]],
+            [state[2],  state[5], state[8]],
+            [state[0],  state[1], state[2]],
+            [state[3],  state[4], state[5]],
+            [state[6],  state[7], state[8]],
+            [state[0], state[4], state[8]],
+            [state[2], state[4], state[6]]
+        ];
+
+        let winSubArray = winningPossibilities.find(allEqual);
+        winSubArray? winSubArray = winSubArray.join(""): false;
+
+        if (winSubArray === "XXX" || winSubArray === "OOO") {
+            return true;
         } else {
             return false;
         }
@@ -49,7 +52,7 @@ const gameboard = (() => {
             return true;
         }
     }
-    return {state, isGameWon, isSlotFree, printGameState};
+    return {state, isGameWon, isSlotFree, resetGame};
 
 })();
 
@@ -57,41 +60,102 @@ const gameboard = (() => {
 function createPlayer (name, symbol, goFirst) {
     let isMyTurn = goFirst;
 
-    const playMove = function () {
-        let slot = +prompt("Where would you like to place your move?", "");
-
-        if (gameboard.isGameWon() === false &&
-            gameboard.isSlotFree(slot) === true) {
-            console.log("playing a move");
-            gameboard.state[slot] = symbol;
-        } else if (gameboard.isGameWon() === true) {
-            console.log("NOT playing a move");
-            console.log("GAME OVER");
-            console.log(`${name} with symbol ${symbol} won!`);
-        }
+    const playMove = function (slot) {
+        gameboard.state[slot] = symbol;
     }
-    return {playMove, isMyTurn};
+    return {playMove, isMyTurn, name, symbol};
+}
+
+function drawGame () {
+
+    const cellList = document.querySelectorAll(".cell");
+
+    for (let i = 0; i < cellList.length; i++) {
+        let correctSymbol = gameboard.state[i];
+        cellList[i].textContent = correctSymbol;
+    }
 }
 
 
 const gameFlow = (() => {
+    const page1 = document.querySelector(".page1");
+    const page2 = document.querySelector(".page2");
+    const page = document.querySelector("body");
+    const start = document.querySelector(".play");
 
-    const player1 = createPlayer("Mike", "X", true);
-    const player2 = createPlayer("Luna", "O", false);
-    gameboard.printGameState();
 
-    let coinFlip;
-    Math.random() < 0.5? coinFlip = true: coinFlip = false;
+    page2.remove();
+    let player1name, player2name;
+    let player1, player2;
+    start.addEventListener("click", () => {
+        player1name = document.getElementById("name1").value;
+        player2name = document.getElementById("name2").value;
+        page1.remove();
+        page.appendChild(page2);
 
-    const swapTurns = function () {
-        player1.isMyTurn = !player1.isMyTurn;
-        player2.isMyTurn = !player2.isMyTurn;
-    }
+        drawGame();
+        player1 = createPlayer(player1name, "X", true);
+        player2 = createPlayer(player2name, "O", false);
 
-    while (gameboard.isGameWon() === false) {
-        player1.isMyTurn? player1.playMove(): player2.playMove();
-        gameboard.printGameState();
-        // draw game here
-        swapTurns();
-    }
+        const gameinfo = document.querySelector(".gameinfo");
+        gameinfo.textContent = `${player1.name}'s turn`;
+
+
+        const swapTurns = function () {
+            player1.isMyTurn = !player1.isMyTurn;
+            player2.isMyTurn = !player2.isMyTurn;
+        }
+
+        let turncount = 0;
+
+        const cellList = document.querySelectorAll(".cell");
+
+        const resetTheGame = function () {
+            turncount = 0;
+            player1.isMyTurn = true;
+            player2.isMyTurn = false;
+            const resetButton = document.createElement("button");
+            resetButton.textContent = "RESET";
+            resetButton.classList.add("resetButton");
+            page2.appendChild(resetButton);
+
+            resetButton.addEventListener("click", () => {
+                gameboard.resetGame();
+                drawGame();
+                gameinfo.textContent = `${player1.name}'s turn`;
+                resetButton.remove();
+            });
+        }
+
+        cellList.forEach((item) => {
+            item.addEventListener("click", (e) => {
+                let nr = item.dataset.indexNumber;
+                if (gameboard.isGameWon() === false && gameboard.isSlotFree(nr) === true) {
+                    if (player1.isMyTurn) {
+                        player1.playMove(nr);
+                        gameinfo.textContent = `${player2.name}'s turn, Symbol: ${player2.symbol}`;
+                        turncount++;
+                        swapTurns();
+                    } else {
+                        player2.playMove(nr);
+                        gameinfo.textContent = `${player1.name}'s turn, Symbol: ${player1.symbol}`;
+                        turncount++;
+                        swapTurns();
+                    }
+                    if (gameboard.isGameWon() === true) {
+                        if (turncount % 2 === 0) {
+                            gameinfo.textContent = `${player2.name} won!`;
+                        } else {
+                            gameinfo.textContent = `${player1.name} won!`;
+                        }
+                        resetTheGame();
+                    } else if (gameboard.isGameWon() === false && gameboard.state.includes(null) === false) {
+                        gameinfo.textContent = `A tie!`;
+                        resetTheGame();
+                    }
+                    drawGame();
+                }
+            })
+        })
+    });
 })();
